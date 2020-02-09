78b1651dd566e8a5ec2db4ce4ded5670f34df51f
CMooreCode <moorecodymail@gmail.com>
2020-02-09 10:36:43 -0500

    dumb

--- game.js
+++ game.js
@@ -164,7 +164,7 @@ function collide(objId, objId2){
 function cleanup(){
     for(let objId in world.objects){
         if(world.objects[objId].type == 2 || world.objects[objId].type == 4) {
-            if(world.objects[objId].x > WORLD_SIZE + 50 || world.objects[objId].x < -WORLD_SIZE - 50 || world.objects[objId].y > WORLD_SIZE + 50 || world.objects[objId].x < -WORLD_SIZE - 50){
+            if(world.objects[objId].x > WORLD_SIZE + 50 || world.objects[objId].x < -WORLD_SIZE - 50 || world.objects[objId].y > WORLD_SIZE + 50 || world.objects[objId].y < -WORLD_SIZE - 50){
                 if(world.objects[objId].type == 4){
                     asteroidCount--;
                 }
